dd3fdef528baf71280fe74f61259ae650f9f449b
Kaustubh Gadhave <73011416+kaustubh-alt@users.noreply.github.com>
2025-08-10 20:05:04 +0530

    Delete glowminds directory

--- glowminds/eatright/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import userdb , useraction , userfavoritedishes
-
-admin.site.register(userdb)
-admin.site.register(useraction)
-admin.site.register(userfavoritedishes)
